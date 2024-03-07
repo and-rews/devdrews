de84b232fb625232f7d05fb4feea485d21406d4c
and-rews <oseiandrews394@gmail.com>
2024-03-07 13:43:18 +0000

    about page completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,34 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Projects", path: "/projects" },
+  { id: 4, name: "Blog", path: "/blog" },
+  { id: 5, name: "Contact", path: "/contact" },
+];
 
 const VerticalNavbar = () => {
+  const pathname = usePathname();
+  const isActive = (path) => path === pathname;
   return (
     <nav className="nav">
       <ul className="text-white flex space-y-4">
-        <li>
-          <Link href="/" className="mr">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="mr">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects" className="mr">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog" className="mr">
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className="mr">
-            Contact
-          </Link>
-        </li>
+        {navLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <Link
+                href={link.path}
+                className={isActive(link.path) ? "active mr" : "mr"}
+              >
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );

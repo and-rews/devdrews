103afb93e1d97af745647b96386d61a8413c2268
and-rews <oseiandrews394@gmail.com>
2024-03-13 16:54:38 +0000

    scroll effect fixed

--- components/TransitionEffect.js
+++ components/TransitionEffect.js
@@ -2,23 +2,17 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const TransitionEffect = ({ children }) => {
   return (
-    <AnimatePresence>
+    <AnimatePresence initial={true}>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 3 }}
         style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
           width: "100%",
           height: "100%",
-          background: "linear-gradient(to bottom, black, #062200)",
+          background: "linear-gradient(to bottom, #00000054, #0E4B0021)",
         }}
-        onAnimationComplete={() => {
-          document.querySelector(".overlay").style.transition = "none";
-        }}
-        className="overlay"
       >
         {children}
       </motion.div>

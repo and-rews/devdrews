ceae76edf3576c3e5bd48bdb39a83b6838581b9c
And-rews <oseiandrews394@gmail.com>
2024-06-05 21:22:08 +0000

    active links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,17 +6,47 @@ import "aos/dist/aos.css";
 import styles from "../styles/Navbar.module.css";
 import Link from "next/link";
 import { FolderOpen, Home, PhoneCall, Rss, User } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Home", href: "/", icon: <Home /> },
+  { name: "About", href: "/about", icon: <User /> },
+  { name: "Projects", href: "/projects", icon: <FolderOpen /> },
+  { name: "Blog", href: "/blog", icon: <Rss /> },
+  { name: "Contact", href: "/contact", icon: <PhoneCall /> },
+];
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   useEffect(() => {
     AOS.init({
-      duration: 1000, // Animation duration
+      duration: 1000,
     });
   }, []);
 
   return (
     <div className={styles.navContainer}>
       <aside data-aos="fade-left" className={styles.navbar}>
+        <nav className={styles.nav}>
+          {navLinks.map((link) => {
+            const isActive = pathname.endsWith(link.href);
+            return (
+              <ul key={link.name} className={styles.ul}>
+                <li data-aos="fade-right" data-aos-delay="100">
+                  <Link href={link.href}>
+                    <i className={isActive ? "text-orange-700" : "text-white"}>
+                      {link.icon}
+                    </i>
+                    <span>{link.name}</span>
+                  </Link>
+                </li>
+              </ul>
+            );
+          })}
+        </nav>
+      </aside>
+      {/* <aside data-aos="fade-left" className={styles.navbar}>
         <nav className={styles.nav}>
           <ul className={styles.ul}>
             <li data-aos="fade-right">
@@ -51,7 +81,7 @@ export default function Navbar() {
             </li>
           </ul>
         </nav>
-      </aside>
+      </aside> */}
     </div>
   );
 }

66fe67af0b25a0762f42e739a15893acbbffd7c3
And-rews <oseiandrews394@gmail.com>
2024-08-14 11:40:57 +0000

    sitemap

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -1,13 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// app/api/sitemap/route.ts
+import { NextResponse } from "next/server";
 import { SitemapStream, streamToPromise } from "sitemap";
 import { Readable } from "stream";
-import { db } from "../../firebase";
+import { db } from "@/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function GET() {
   // Fetch dynamic pages (e.g., blog posts)
   const blogPosts = await getDocs(collection(db, "blogs"));
   const blogUrls = blogPosts.docs.map((doc) => ({
@@ -22,17 +20,18 @@ export default async function handler(
     { url: "/projects", changefreq: "weekly", priority: 0.8 },
     { url: "/blog", changefreq: "daily", priority: 0.9 },
     ...blogUrls,
+    // Add more static pages as needed
   ];
 
   const stream = new SitemapStream({ hostname: "https://devdrews.com" });
-
-  res.writeHead(200, {
-    "Content-Type": "application/xml",
-  });
 
   const xmlString = await streamToPromise(
     Readable.from(pages).pipe(stream)
   ).then((data) => data.toString());
 
-  res.end(xmlString);
+  return new NextResponse(xmlString, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
